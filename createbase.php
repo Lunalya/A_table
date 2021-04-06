8fe07be2f0ed4f38ed5ecf9cbd6a50719111a07b
Wendy <74055268+Lunalya@users.noreply.github.com>
2021-04-06 16:12:52 +0200

    Add files via upload

---
+++ createbase.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>
+        <?php
+            $servername = 'localhost';
+            $dbname= 'a_table';
+            $username = 'root';
+            $password = 'Canelle1995';
+
+            try{
+                $dbco = new PDO("mysql:host=$servername", $username, $password);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                $sql = "CREATE DATABASE a_table";
+                $dbco->exec($sql);
+
+                echo 'Base de données créée bien créée !';
+            }
+
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+
+
+            try{
+                            $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                            $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                            $table_client = "CREATE TABLE clients(
+                                    numQR INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                                    smartphone VARCHAR(30) NOT NULL,
+                                    langue VARCHAR(30) NOT NULL,
+                                    dates DATETIME NOT NULL)";
+
+                            $table_tables = "CREATE TABLE orga_tables(
+                                    numTABLES INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                                    emplacement VARCHAR(30) NOT NULL,
+                                    situation VARCHAR(30) NOT NULL,
+                                    chaises INT NOT NULL)";
+
+                            $table_qrCodes = "CREATE TABLE qr_codes(
+                                    numQR INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                                    nom VARCHAR(30) NOT NULL,
+                                    numTABLES INT NULL)";
+
+
+                            $dbco->exec($table_client);
+                            $dbco->exec($table_tables);
+                            $dbco->exec($table_qrCodes);
+                            echo 'Table bien créée !';
+                        }
+
+                        catch(PDOException $e){
+                          echo "Erreur : " . $e->getMessage();
+                        }
+
+
+
+?>
+    </body>
+</html>
